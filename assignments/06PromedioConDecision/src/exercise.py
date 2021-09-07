d77686a65e505bede46ce78b766092096ed591e7
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-09-07 16:08:00 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,16 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
+##promedio
+suma=0
+num=float(input('ingrese los digitos'))
+cont=0
+while num >0:
+       suma= suma+num
+       cont=cont+1
+       num=float(input('ingrese los digitos'))
+promedio=suma/cont
+    
+print(promedio)
 if __name__=='__main__':
     main()
