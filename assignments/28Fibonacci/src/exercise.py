855dc4c8dd127d43362426f3eca02a22ce6d304d
rabigail26 <89092963+rabigail26@users.noreply.github.com>
2021-09-07 16:04:35 -0500

    Update exercise.py

--- assignments/28Fibonacci/src/exercise.py
+++ assignments/28Fibonacci/src/exercise.py
@@ -3,5 +3,13 @@ def main():
     #escribe tu código abajo de esta línea
     pass
 
+indice= int(input("Dame el índice: "))
+suma=0
+inicio=1
+while indice > 0:
+    indice = indice-1
+    suma=suma+inicio
+    inicio=suma-inicio
+print (suma)
 if __name__=='__main__':
     main()
